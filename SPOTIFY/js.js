46484fc315e1dcbfe2dd0bbf7fe468eb41ed1851
Naman Yadav <yadavnaman99967@gmail.com>
2024-12-13 20:46:03 +0530

    Add files via upload

---
+++ SPOTIFY/js.js
@@ -0,0 +1,8 @@
+let a = document.getElementById("box1");
+a.addEventListener("click", function() {
+    window.location.href = "playlist1.html";
+});
+let b = document.getElementById("box2");
+b.addEventListener("click", function() {
+    window.location.href = "playlist2.html";
+});
